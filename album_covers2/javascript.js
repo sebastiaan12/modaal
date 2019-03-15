c11a3abd906a0039a28fa5afe069f0301ba0d61a
sebastiaan12 <43434484+sebastiaan12@users.noreply.github.com>
2019-03-15 19:49:33 +0100

    Add files via upload

---
+++ album_covers2/javascript.js
@@ -0,0 +1,49 @@
+
+const modaalContent = document.querySelectorAll('.modaalContent');
+
+for(let i=0; i<modaalContent.length; i++) {
+  let modaalNode = modaalContent[i];
+  modaalNode.parentNode.removeChild(modaalNode);
+}
+const modaalKnoppen = document.querySelectorAll('.modaal-knop');
+const modaalKnoppenArray = [];
+
+
+let modaalAchtergrond = document.createElement('div');
+modaalAchtergrond.className = 'modaal-achtergrond';
+let modaal = document.createElement('div');
+modaal.className = 'modaal';
+let sluitKnop = document.createElement('knop');
+sluitKnop.className = 'sluit-knop';
+sluitKnop.innerHTML = '&#x00D7;';
+const voegInhoudToe = (event) => {
+  const teller = modaalKnoppenArray.indexOf(event.target);
+ 
+
+  console.log(teller);
+  modaal.appendChild(sluitKnop);
+  modaal.appendChild(modaalContent[teller]);
+  modaalAchtergrond.appendChild(modaal);
+  document.body.appendChild(modaalAchtergrond);
+}
+
+for(let i=0; i<modaalKnoppen.length; i++) {
+ 
+  modaalKnoppenArray.push(modaalKnoppen[i]);
+
+  modaalKnoppen[i].addEventListener('click', voegInhoudToe);
+}
+
+
+
+
+
+const sluitModaal = () => {
+  modaal.innerHTML = '';
+  modaalAchtergrond.innerHTML = '';
+  document.body.removeChild(modaalAchtergrond);
+}
+
+sluitKnop.addEventListener('click', sluitModaal)
+
+
